8fbc4480d2a95a9c5399abc8a1207463debef7c9
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-05-29 03:53:43 +0700

    fix: use router instead of hard-redirect on auth failed

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,15 @@
-export default function({ store, redirect }) {
-  return store
+export default function({ app, redirect }) {
+  if (app.store.state.auth.isAuth) {
+    return Promise.resolve(true)
+  }
+  return app.store
     .dispatch('auth/checkExistingToken')
     .then(() => {
       return true
     })
     .catch(() => {
-      redirect('/login')
+      return app.router.replace({
+        path: '/login',
+      })
     })
 }
