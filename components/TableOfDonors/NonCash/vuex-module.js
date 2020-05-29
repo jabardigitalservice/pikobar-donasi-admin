4d361242d7eb3dfa20a6b3bb39f8a62867b9b52c
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-05-30 00:46:07 +0700

    fix: ensure table data is refresh on verification status changed;

--- components/TableOfDonors/NonCash/vuex-module.js
+++ components/TableOfDonors/NonCash/vuex-module.js
@@ -22,16 +22,11 @@ const mutations = {
 const actions = {
   async getListOfDonors({ state, getters, commit }, options = {}) {
     const mOptions = {
-      refresh: false,
       perPage: state.perPage,
       page: 1,
       ...options,
     }
-    if (
-      !Array.isArray(state.listOfDonors) ||
-      !state.listOfDonors.length ||
-      mOptions.refresh
-    ) {
+    if (!Array.isArray(state.listOfDonors) || !state.listOfDonors.length) {
       const docs = await getListOfNonCashDonors({
         perPage: mOptions.perPage,
         page: mOptions.page,
